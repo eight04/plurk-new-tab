b2c3d5c5ae308a7bc5975c419432d2f76fddc8f6
eight04 <eight04@gmail.com>
2026-02-01 15:41:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Plurk in new tab
+================
+
+[![.github/workflows/build.yml](https://github.com/eight04/plurk-new-tab/actions/workflows/build.yml/badge.svg)](https://github.com/eight04/plurk-new-tab/actions/workflows/build.yml)
+
+在手機版plurk中，從使用者頁面跳轉到噗文後，回到使用者頁面時，會重新載入使用者頁面，導致使用者體驗不佳。此 userscript 會讓噗文在新分頁開啟，避免此問題。
+
+Install
+-------
+
+TODO
+
+Changelog
+---------
+
+* 0.1.0 (Feb 1, 2026)
+
+	- First release.
---
+++ plurk-new-tab.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        Plurk in new tab
+// @description	將 Plurk 連結開啟於新分頁
+// @namespace   https://github.com/eight04
+// @match       https://www.plurk.com/m/*
+// @exclude-match https://www.plurk.com/m/p/*
+// @exclude-match https://www.plurk.com/m/search*
+// @version     0.0.0
+// @author		eight <eight04@gmail.com> (https://github.com/eight04)
+// @homepage	https://github.com/eight04/plurk-new-tab
+// @supportURL	https://github.com/eight04/plurk-new-tab/issues
+// @license		MIT
+// @compatible firefox Tampermonkey, Violentmonkey, Greasemonkey 4.11+
+// @compatible chrome Tampermonkey, Violentmonkey
+// @grant       GM_openInTab
+// ==/UserScript==
+
+unsafeWindow.addEventListener("click", e => {
+  let url;
+  let el;
+
+  if ((el = e.target.closest("a.pictureservices"))) {
+    // img link
+    url = el.href;
+  } else if ((el = e.target.closest("[class*=button], a"))) {
+    // btn, other links
+    return;
+  } else if ((el = e.target.closest(".plurk"))) {
+    // plurk
+    const {plurkId} = el.__vue__.$options.propsData;
+    url = new URL(`p/${plurkId.toString(36)}`, location.href).href
+  }
+
+  if (url) {
+    e.preventDefault();
+    e.stopPropagation();
+    GM_openInTab(url, {active: true});
+  }
+}, true);
